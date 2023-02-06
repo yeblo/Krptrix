5aaae4436bd2fa3ff39e1897fc8f48e08bfaf6f2
yeblo <yeblo95@yahoo.com>
2023-02-06 03:10:29 -0600

    includes mail chimp integration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 // import NavBar from './nav';
 import { MDBBtn, MDBContainer, MDBIcon } from 'mdb-react-ui-kit';
 import './App.css';
-import krptrix_logo from './krptrix_logo.svg';
+import krptrix_logo from './krptrix_logo.png';
 import krptrix_logo_footer from './krptrix_logo_footer.svg';
 import right_arrow from './right_arrow.svg';
-function App() {
+import MailchimpFormContainer from './mailchimp';
+function App({ status, message, onValidated }) {
+  const [email, setEmail] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  useEffect(() => {
+    if (status === "success") clearFields();
+  }, [status])
+
+  const clearFields = () => {
+    setFullName('');
+    setEmail('');
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+      fullName &&
+      email.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email,
+        MERGE1: fullName
+      });
+  }
+
   return (
     <React.Fragment>
       <header className='header' style={{ height: '100%' }}>
@@ -19,9 +42,9 @@ function App() {
           <div className='mask'>
             <div className='d-flex justify-content-center align-items-center h-100'>
               <div className='hero container'>
-                <h1 className='mb-3 hero-heading'>The ultimate web3 platform for <br /> resources in design and development</h1>
-                <p className='mb-3 hero-subheading'>Curious on how to begin your Web3 journey? Krptrix will help you create an easy roadmap to start and grow your career in web3</p>
-                <MDBBtn tag="a" className="hero-button mt-5" href="#waitlist">Join the waitlist <MDBIcon className="px-1" fas icon="long-arrow-alt-right" /></MDBBtn>
+                <h1 className='mb-3 hero-heading'>The ultimate platform for resources <br/>in web3 design and development</h1>
+                <p className='mb-3 hero-subheading pt-2'>Become a certified, badass web3 designer/developer. Make the most of <br/>opportunities in the web3 space and stay up to date with the latest news and trends</p>
+                <MDBBtn tag="a" className="hero-button mt-5" href="#waitlist">Join the waitlist <span className='px-2'><img src={right_arrow} alt="right arrow"/> </span></MDBBtn>
               </div>
             </div>
           </div>
@@ -31,36 +54,7 @@ function App() {
 
         <MDBContainer className='py-5'>
           <h3 className="text-center form-title mb-3">Get Notified when we launch!</h3>
-          <form className='form' action='https://gmail.us21.list-manage.com/subscribe/post?u=d4c1ce45bff8c42d896599840&amp;id=e326a49a5f&amp;f_id=0078c4e1f0' method='post'>
-
-            <div className='form-group mb-4'>
-              <label for="email" className='mb-1' name="EMAIL" id="mce-EMAIL">Email Address</label>
-              <input type="text" placeholder='Email Address' required />
-            </div>
-
-            <div className='form-group mt-2'>
-              <label for="name" className='mb-1'>Full Name</label>
-              <input type="text" placeholder='Full Name' name="FNAME" id="mce-FNAME" required />
-            </div>
-
-            <div id="mce-responses" class="clear foot">
-              <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
-              <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
-              </div> {/*  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-            <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true">
-              <input type="text" name="b_d4c1ce45bff8c42d896599840_e326a49a5f" tabindex="-1" value=""/>
-              </div>
-
-            <div className='text-center'>
-              <MDBBtn tag="a" className="hero-button mt-5"  name="subscribe" id="mc-embedded-subscribe">Join the waitlist <img src={right_arrow} alt="right arrow" />
-                {/* <MDBIcon className="px-1" fas icon="long-arrow-alt-right" /> */}
-              </MDBBtn>
-            </div>
-
-
-          </form>
-
-
+          <MailchimpFormContainer/>
         </MDBContainer>
       </section>
 
@@ -73,15 +67,15 @@ function App() {
             />
           </div>
 
-          <p className='mt-3'>The ultimate web3 platform for resources in design and development</p>
+          <p className='mt-3'>The ultimate platform for resources in web3 design and development</p>
           <div className='mt-3'>
             <a className='sm-icon' href="#"><MDBIcon className="mx-3" fab icon="facebook-f" /></a>
             <a className='sm-icon' href="https://www.instagram.com/krptrix/"><MDBIcon className="mx-3" fab icon="instagram" /></a>
             <a className='sm-icon' href="https://twitter.com/krptrix"><MDBIcon className="mx-3" fab icon="twitter" /></a>
             <a className='sm-icon' tag="a" href="#" ><MDBIcon className="mx-3" fab icon="linkedin-in" /></a>
           </div>
-          <hr />
-          <p>Copyright © 2022, Krptrix | All rights reserved.</p>
+          <hr className="footer-hr" />
+          <p className='for-copywrite'>Copyright © 2022, Krptrix | All rights reserved.</p>
         </MDBContainer>
       </footer>
     </React.Fragment>
--- my-app/src/mailchimp.js
+++ my-app/src/mailchimp.js
@@ -1,11 +1,122 @@
-import React from 'react';
-import './mcFormStyles.scss';
+import React, { useState, useEffect } from 'react';
+// import './mcFormStyles.scss';
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import InputField from './ui/InputField';
+
+const CustomForm = ({ status, message, onValidated }) => {
+    const [email, setEmail] = useState('');
+    const [fullName, setFullName] = useState('');
+
+    useEffect(() => {
+        if(status === "success") clearFields();
+      }, [status])
+    
+      const clearFields = () => {
+        setFullName('');
+        setEmail('');
+      }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        email &&
+            fullName &&
+            email.indexOf("@") > -1 &&
+            onValidated({
+                EMAIL: email,
+                MERGE1: fullName
+            });
+    }
+
+    return (
+        <form className="form" onSubmit={(e) => handleSubmit(e)}>
+            <h3 className="mc__title"> {status === "success"
+                ? "Success!"
+                : ""
+            }</h3>
+
+            {status === "sending" && (
+                <div className="mc__alert mc__alert--sending">
+                    sending...
+                </div>
+            )}
+            {status === "error" && (
+                <div
+                    className="mc__alert mc__alert--error"
+                    dangerouslySetInnerHTML={{ __html: message }}
+                />
+            )}
+            {status === "success" && (
+                <div
+                    className="mc__alert mc__alert--success"
+                    dangerouslySetInnerHTML={{ __html: message }}
+                />
+            )}
+
+            {status !== "success" ? (
+                <div className="mc__field-container"> </div>
+            ) : null}
+
+            {/* 
+             implement modal here
+            {
+                status === 'success' ? <button
+                    // onClick={() => setModalOpen(false)}
+                    className="g__justify-self-center">Close</button> : <InputField
+                    label="subscribe"
+                    type="submit"
+                    formValues={[email, fullName]}
+                />
+            } */}
+
+            <div className='form-group mb-4'>
+                <InputField
+                    label="Email"
+                    onChangeHandler={setEmail}
+                    type="email"
+                    value={email}
+                    placeholder="Enter Email Address"
+                    html
+                    isRequired
+                />
+            </div>
+            <div className='form-group mt-2' >
+                <InputField
+                    label="Full Name"
+                    onChangeHandler={setFullName}
+                    type="text"
+                    value={fullName}
+                    placeholder="Enter Full Name"
+                    isRequired
+                />
+            </div>
+
+            <InputField
+                className=""
+                label="Join the waitlist &#x2192;"
+                type="submit"
+                formValues={[email, fullName]}
+            />
+        </form>
+    );
+};
 
 const MailchimpFormContainer = props => {
+    const action_url = "https://gmail.us21.list-manage.com/subscribe/post?u=d4c1ce45bff8c42d896599840&amp;id=e326a49a5f&amp;f_id=0078c4e1f0";
+    // const postUrl = `https://genhybridsystems.us1.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+    // const u = "";
+    // const id = "";
     return (
         <div className="mc__form-container">
-            <MailchimpSubscribe />
+            <MailchimpSubscribe
+                url={action_url}
+                render={({ subscribe, status, message }) => (
+                    <CustomForm
+                        status={status}
+                        message={message}
+                        onValidated={formData => subscribe(formData)}
+                    />
+                )}
+            />
         </div>
     );
 };
